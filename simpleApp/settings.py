0dde634005e097a124e764837bbcb081543119c0
Ahmad S. Salama <55457021+vadrif-draco@users.noreply.github.com>
2021-10-08 22:00:22 +0200

    All hosts allowed for dev purposes

--- simpleApp/settings.py
+++ simpleApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qmrt%#_z$77dlllgt*4786tc=cn_3$+55k&a=o24pyay)4bv&+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # FIXME: For dev puproses, DO NOT go prod like this!
 
 
 # Application definition
